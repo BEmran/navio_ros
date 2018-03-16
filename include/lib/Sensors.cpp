059c8a66755937a4e02d2b0d8c5f5dba69992204
Bara Emran <bara.emran@gmail.com>
2018-03-16 12:07:03 -0700

    add Encoder and Sensor library

---
+++ include/lib/Sensors.cpp
@@ -0,0 +1,134 @@
+#include "Sensors.h"
+
+Sensors::Sensors (){}
+
+
+Sensors::Sensors (std::string sensor_name, bool debug = true){
+    is_debug = debug;
+    bias.gx = 0.0;
+    bias.gy = 0.0;
+    bias.gz = 0.0;
+
+    // get current time
+    getTime();
+
+    if (sensor_name == "mpu") {
+        printf("Selected: MPU9250\n");
+        is = new MPU9250();
+    }
+    else if (sensor_name == "lsm") {
+        printf("Selected: LSM9DS1\n");
+        is = new LSM9DS1();
+    }
+    else {
+        is = NULL;
+    }
+
+    // Create a file to store the row data
+    if (is_debug){
+        char file_name[128];
+        sprintf(file_name,"row_data_%s.txt", sensor_name.c_str());
+        row_data_file = fopen(file_name, "w");
+        printf("Start storing the data in the file \"%s\"\n",file_name);
+        fprintf(row_data_file, "time, ax, ay, az, gx, gy, gz, mx, my, mz\n");
+    }
+
+    // Initilaize imu sensor and calibrate gyro
+    is->initialize();
+    isISEnabled = is->probe();
+    if (isISEnabled){
+        calibrateGyro();
+    }
+}
+
+void Sensors::update(){
+    is->update();
+    is->read_accelerometer(&imu.ax, &imu.ay, &imu.az);
+    is->read_gyroscope(&imu.gx, &imu.gy, &imu.gz);
+    is->read_magnetometer(&imu.mx, &imu.my, &imu.mz);
+    // rotate axis
+    float tmpax = imu.ax;
+    float tmpgx = imu.gx;
+    float tmpmx = imu.mx;
+    imu.ax = -imu.ay;
+    imu.gx = -imu.gy;
+    imu.mx = -imu.my;
+    imu.ay = -tmpax;
+    imu.gy = -tmpgx;
+    imu.my = -tmpmx;
+
+    // apply calibration
+    imu.ax /= G_SI;
+    imu.ay /= G_SI;
+    imu.az /= G_SI;
+
+    imu.gx -= bias.gx;
+    imu.gy -= bias.gy;
+    imu.gz -= bias.gz;
+
+    // store data
+    if (is_debug){
+        storeData();
+    }
+
+}
+
+//**************************************************************************
+// Calibrate Gyroscope sensor: find bias values
+//**************************************************************************
+void Sensors::calibrateGyro()
+{
+    //---------------------- Calculate the offset -----------------------------
+    float offset[3] = {0.0, 0.0, 0.0};
+
+    //-------------------------------------------------------------------------
+    printf("Beginning Gyro calibration...\n");
+    for(int i = 0; i<500; i++)
+    {
+        update();
+
+        imu.gx *= 180 / PI;
+        imu.gy *= 180 / PI;
+        imu.gz *= 180 / PI;
+
+        offset[0] += imu.gx*0.0175;
+        offset[1] += imu.gy*0.0175;
+        offset[2] += imu.gz*0.0175;
+
+        usleep(10000);
+    }
+    offset[0]/=500.0;
+    offset[1]/=500.0;
+    offset[2]/=500.0;
+
+    printf("Offsets are: %+10.5f %+10.5f %+10.5f\n", offset[0], offset[1], offset[2]);
+
+    bias.gx = offset[0];
+    bias.gx = offset[1];
+    bias.gx = offset[2];
+}
+//**************************************************************************
+// Store row measurements
+//**************************************************************************
+
+void Sensors::storeData() {
+    // get current time
+    getTime();
+    // Write data
+    fprintf(row_data_file, "%12lu,",time_now);
+    fprintf(row_data_file, " %+10.5f,  %+10.5f,  %+10.5f,",imu.ax, imu.ay, imu.az);
+    fprintf(row_data_file, " %+10.5f,  %+10.5f,  %+10.5f,",imu.gx, imu.gy, imu.gz);
+    fprintf(row_data_file, " %+10.5f,  %+10.5f,  %+10.5f\n",imu.mx, imu.my, imu.mz);
+}
+//**************************************************************************
+// Get the current time
+//**************************************************************************
+
+void Sensors::getTime() {
+    // Timing data
+    struct timeval tv;
+
+    // Calculate delta time
+    gettimeofday(&tv, NULL);
+    time_now = 1000000 * tv.tv_sec + tv.tv_usec;
+}
---
+++ include/lib/Sensors.h
@@ -0,0 +1,45 @@
+#ifndef SENSORS_H
+#define SENSORS_H
+
+#include "Common/MPU9250.h"
+#include "Navio2/LSM9DS1.h"
+#include "Common/Util.h"
+#include <unistd.h>
+#include <string>
+#include <stdio.h>	// file, printf
+#include <sys/time.h>	// time
+
+namespace {
+    #define G_SI 9.80665
+    #define PI   3.14159
+}
+
+struct imu_struct{
+    float ax, ay, az;
+    float gx, gy, gz;
+    float mx, my, mz;
+};
+
+class Sensors{
+
+public:
+    bool isISEnabled;
+    struct imu_struct imu;
+    struct imu_struct bias;
+
+    Sensors ();
+    Sensors (std::string sensor_name, bool debug);
+    void update();
+    void calibrateGyro();
+
+private:
+    bool is_debug;
+    long unsigned time_now;
+    InertialSensor *is;
+    FILE * row_data_file;   // File to store row data
+
+    void storeData();
+    void getTime();
+};
+
+#endif //SENSORS_H
