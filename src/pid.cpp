cb5d94fb8999b1cf4c4bea9c8a3fd19dcde4735b
Bara Emran <bara.emran@gmail.com>
2018-10-02 12:02:09 -0700

    rotor pid

--- src/pid.cpp
+++ src/pid.cpp
@@ -1,61 +1,102 @@
 #include "../include/testbed_navio/navio_interface.h"
-#include "../include/lib/TimeSampling.h"               // time sampling library
+#include "../include/lib/TimeSampling.h"                // time sampling library
+#include "../include/lib/ode.h"                         // ODE library
 #include <iostream>
 #include <signal.h>                         // signal ctrl+c
 #include "ros/ros.h"
-#include "geometry_msgs/TwistStamped.h"     // du msg
+#include "geometry_msgs/Twist.h"     // du msg
 #include "geometry_msgs/Vector3Stamped.h"   // encoder and RPY msg
-
-void pid(float ang[], float des[], float du[]);
 /**************************************************************************************************
  *
 **************************************************************************************************/
-class ROSNODE{
-protected:
-  int _queue_size;
-  ros::NodeHandle _nh;
-  ros::Publisher _pub_ang, _pub_du;    // publish angle encoder message
-  ros::Subscriber _sub_cmd;    // subscriber to desired duty cycle
-  std::string _name;
+vec dyn(vec& x, vec& xdot, vec& u, vec& par){
+  vec y = x;
+  xdot[0] = x[1];
+  xdot[1] = - 300*x[0] - 35*x[1] + 35*u[0];
+  return y;
+}
+class Rotor{
+private:
+  ODE ode;
+  float ei;
+  float dt;
+  vec x;
 public:
-  float _cmd[4];
-  ROSNODE(){}
-  ROSNODE(ros::NodeHandle nh, std::string name){
-    _cmd[0] = 0.0; _cmd[1] = 0.0; _cmd[2] = 0.0;
-    _nh = nh;
-    _name = name;
-    _queue_size = 10;
-    _pub_ang = _nh.advertise <geometry_msgs::Vector3Stamped>("encoders", _queue_size);
-    _pub_du  = _nh.advertise <geometry_msgs::TwistStamped>("du", _queue_size);
-    _sub_cmd = _nh.subscribe("cmd", _queue_size, &ROSNODE::cmdCallback, this);
 
+  Rotor (float Dt){
+    dt = Dt;
+    ei = 0;
+    ode = ODE(2, dyn);
+    x = ode.getX();
   }
-  ~ROSNODE(){}
-  void publishAngMsg(const float ang[3]){
-    geometry_msgs::Vector3Stamped msg_ang;
-    msg_ang.header.stamp = ros::Time::now();
-    msg_ang.header.seq++;
-    msg_ang.vector.x = ang[0];
-    msg_ang.vector.y = ang[1];
-    msg_ang.vector.z = ang[2];
-    _pub_ang.publish(msg_ang);
+  Rotor(){}
+  ~Rotor(){}
+  float update(float RPMdes){
+    // calculate error
+    float e = (RPMdes/10000) - x[0];
+    // PI conrol with anti windup procedure
+    float Kp = 2.5, Ki = 20.5, Kt = 0.1;
+    float u = Kp * e + ei;
+    float usat;
+    if (u > 1000)
+        usat = 1000;
+    else if (u < 0)
+      usat = 0;
+    else
+      usat = u;
+    ei = ei + e * dt * (Ki + Kt * (-u+usat));
+    // PWM signal conditioning
+    if (usat > 0)
+      usat = usat * 0.4177 + 0.5;
+    else
+      usat = 0;
+
+    vec empty;
+    vec input={usat};
+    x = ode.update(input, empty, dt);
+
+    return usat;
   }
-  void cmdCallback(const geometry_msgs::Vector3Stamped& msg){
-    ROS_INFO("x=%f, y=%f, z=%f\n",msg.vector.x, msg.vector.y, msg.vector.z);
-    _cmd[0] = msg.vector.x;
-    _cmd[1] = msg.vector.y;
-    _cmd[2] = msg.vector.z;
-  }
-  void publishDuMsg(const float du[4]){
-    geometry_msgs::TwistStamped msg_du;
-    msg_du.header.stamp = ros::Time::now();
-    msg_du.header.seq++;
-    msg_du.twist.linear.z  = du[0];
-    msg_du.twist.angular.x = du[1];
-    msg_du.twist.angular.y = du[2];
-    msg_du.twist.angular.z = du[3];
-    _pub_du.publish(msg_du);
-  }
+};
+
+/**************************************************************************************************
+ *
+**************************************************************************************************/
+class RosNode{
+protected:
+    int _queue_size;
+    ros::NodeHandle _nh;
+    ros::Publisher _pub_ang;    // publish angle encoder message
+    ros::Subscriber _sub_du;    // subscriber to desired duty cycle
+    std::string _name;
+public:
+    float _du[4];
+    RosNode(){}
+    RosNode(ros::NodeHandle nh, std::string name){
+        _du[0] = 0.0; _du[1] = 0.0; _du[2] = 0.0; _du[3] = 0.0;
+        _nh = nh;
+        _name = name;
+        _queue_size = 10;
+        _pub_ang = _nh.advertise <geometry_msgs::Vector3Stamped>("encoders", _queue_size);
+        _sub_du  = _nh.subscribe("du", _queue_size, &RosNode::cmdDuCallback , this);
+    }
+    ~RosNode(){}
+    void publishAngMsg(const float ang[3]){
+        geometry_msgs::Vector3Stamped msg_ang;
+        msg_ang.header.stamp = ros::Time::now();
+        msg_ang.header.seq++;
+        msg_ang.vector.x = ang[0];
+        msg_ang.vector.y = ang[1];
+        msg_ang.vector.z = ang[2];
+        _pub_ang.publish(msg_ang);
+    }
+    void cmdDuCallback(const geometry_msgs::Twist& msg){
+  ROS_INFO("x=%f, z=%f\n",msg.angular.x, msg.angular.z);
+        _du[0] = msg.linear.z;
+        _du[1] = msg.angular.x;
+        _du[2] = msg.angular.y;
+        _du[3] = msg.angular.z;
+    }
 };
 /**************************************************************************************************
  *
@@ -65,98 +106,98 @@ pthread_t _Thread_Control;
 bool _CloseRequested = false;
 void ctrlCHandler(int signal);
 struct dataStruct {
-  bool is_rosnode_ready;
-  float ang[3], du[4];
-  ROSNODE *rosnode;
+    bool is_rosnode_ready;
+    float ang[3];
+    RosNode *rosnode;
 };
 /**************************************************************************************************
  *
 **************************************************************************************************/
 void* controlThread(void *data)
 {
-  // initialization -----------------------------------------------------------------------------
-  printf("Start Control thread\n");
-  struct dataStruct *data_;
-  data_ = (struct dataStruct *) data;
-  PWM *pwm;
-  initializePWM(pwm, 0);
-  setOffPWM(pwm);
-  TimeSampling ts(50);
+    // initialization -----------------------------------------------------------------------------
+    printf("Start Control thread\n");
+    struct dataStruct *data_;
+    data_ = (struct dataStruct *) data;
+    PWM *pwm;
+    initializePWM(pwm, 0);
+    TimeSampling ts(500);
+    Rotor r1(500);
+    // Main loop ----------------------------------------------------------------------------------
+    while (!data_->is_rosnode_ready);
+    float dt, dtsumm = 0;
+    while (!_CloseRequested)
+    {
+        // calculate sampling time
+        dt = ts.updateTs();
 
-  // Main loop ----------------------------------------------------------------------------------
-  while (!data_->is_rosnode_ready);
-  float dt, dtsumm = 0;
-  while (!_CloseRequested)
-  {
-    // calculate sampling time
-    dt = ts.updateTs();
+        // Send PWM
+        float r[4];
+        r[0] = r1.update(data_->rosnode->_du[0]);
+        r[1] = data_->rosnode->_du[1];
+        r[2] = data_->rosnode->_du[2];
+        r[3] = data_->rosnode->_du[3];
+        setPWMDuty(pwm, r);
 
-    // Send PWM
-    printf("u1=%f, u2=%f, u3=%f, u4=%f\n",data_->du[0], data_->du[1], data_->du[2], data_->du[3]);
-    setPWMDuty(pwm, data_->du);
-
-    // Display info for user every 5 second
-    dtsumm += dt;
-    if (dtsumm > 5) {
-      dtsumm = 0;
-      printf("Control thread: running fine with %4d Hz\n", int(1 / dt));
+        // Display info for user every 5 second
+        dtsumm += dt;
+        if (dtsumm > 5) {
+            dtsumm = 0;
+            printf("Control thread: running fine with %4d Hz\n", int(1 / dt));
+        }
     }
-  }
 
-  // Exit procedure -----------------------------------------------------------------------------
-  ctrlCHandler(0);
-  printf("Exit control thread\n");
-  pthread_exit(NULL);
+    // Exit procedure -----------------------------------------------------------------------------
+    ctrlCHandler(0);
+    printf("Exit control thread\n");
+    pthread_exit(NULL);
 }
 /**************************************************************************************************
  *
 **************************************************************************************************/
 void ctrlCHandler(int signal) {
-  _CloseRequested = true;
-  printf("Ctrl+c have been detected\n");
+    _CloseRequested = true;
+    printf("Ctrl+c have been detected\n");
 }
 /**************************************************************************************************
  *
 **************************************************************************************************/
 int main(int argc, char** argv)
 {
-  // initialization -----------------------------------------------------------------------------
-  printf("Start Program\n");
-  dataStruct data;
-  data.is_rosnode_ready = false;
-  data.du[0] = 0.0; data.du[1] = 0.0; data.du[2] = 0.0; data.du[3] = 0.0;
-  signal(SIGINT, ctrlCHandler);
-  pthread_create(&_Thread_Control, NULL, controlThread, (void *) &data);
+    // initialization -----------------------------------------------------------------------------
+    printf("Start Program\n");
+    dataStruct data;
+    data.is_rosnode_ready = false;
+    signal(SIGINT, ctrlCHandler);
+    pthread_create(&_Thread_Control, NULL, controlThread, (void *) &data);
 
-  // Ros node -----------------------------------------------------------------------------------
-  printf("initiate ros node\n");
-  ros::init(argc, argv, "control_test");
-  ros::NodeHandle nh;
-  data.rosnode = new ROSNODE (nh, "control_test");
-  ros::Rate loop_rate(200);
-  Encoder enc(true);
-  data.is_rosnode_ready = true;
-  // Main loop ----------------------------------------------------------------------------------
-  while (ros::ok()){
-    // read encoder and convert it to radian
-    enc.updateCounts();
-    enc.readAnglesRad(data.ang);
+    // Ros node -----------------------------------------------------------------------------------
+    printf("initiate ros node\n");
+    ros::init(argc, argv, "control_test");
+    ros::NodeHandle nh;
+    data.rosnode = new RosNode (nh, "control_test");
+    ros::Rate loop_rate(800);
+    Encoder enc(true);
+    data.is_rosnode_ready = true;
+    // Main loop ----------------------------------------------------------------------------------
+    while (ros::ok()){
+        // read encoder and convert it to radian
+        enc.updateCounts();
+        enc.readAnglesRad(data.ang);
 
-    // publish encoders' angle
-    data.rosnode->publishAngMsg(data.ang);
-    pid(data.ang, data.rosnode->_cmd, data.du);
+        // publish encoders' angle
+        data.rosnode->publishAngMsg(data.ang);
 
-    ros::spinOnce();
-    loop_rate.sleep();
-  }
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
 
-  // Exit procedure -----------------------------------------------------------------------------
-  printf("Close program\n");
-  ctrlCHandler(0);
-  //pthread_cancel(_Thread_Control);
-  return(0);
+    // Exit procedure -----------------------------------------------------------------------------
+    printf("Close program\n");
+    ctrlCHandler(0);
+    //pthread_cancel(_Thread_Control);
+    return(0);
 }
-
 
 /**************************************************************************************************
  *
